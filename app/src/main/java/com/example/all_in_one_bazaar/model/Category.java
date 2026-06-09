b101e23cb0e7d2d0a7cb67c4b6568497050d1577
ahdave1573-dev <ahdave1573@gmail.com>
2026-06-09 14:43:01 +0530

    Added Category data model class

---
+++ app/src/main/java/com/example/all_in_one_bazaar/model/Category.java
@@ -0,0 +1,41 @@
+package com.example.all_in_one_bazaar.model;
+
+public class Category {
+    private String id;
+    private String name;
+    private String image;
+
+    public Category() {
+        // Required empty constructor for Firebase
+    }
+
+    public Category(String id, String name, String image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
